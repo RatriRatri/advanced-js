ab11e2cb99a7ef6e57d9f87e73330f5350bad45e
RatriRatri <ratrikulsum@gmail.com>
2020-03-05 01:21:23 -0800

    true falsy done

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// falsy:
+// const name = " "
+// 0
+// undefined
+// null
+// NaN
+// truthy
+// '0', ''
+let name = 12;
+
+
+if (name || name == 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
